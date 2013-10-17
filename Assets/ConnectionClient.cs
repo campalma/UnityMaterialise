ddc83cec4c00c692c2a15a18bd8ceb0797e1057f
Camilo Palma <cpalmapradena@gmail.com>
2013-10-17 14:16:18 -0300

    Materials query

---
+++ Assets/ConnectionClient.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectionClient : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		MaterialiseConnection connection = MaterialiseConnection.Instance;
+		yield return StartCoroutine(connection.materials());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/MaterialiseConnection.cs
+++ Assets/MaterialiseConnection.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class MaterialiseConnection : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+ private static MaterialiseConnection m_instance;
+        
+    public static MaterialiseConnection Instance{
+            get{
+                    if(m_instance == null)
+                            m_instance = new MaterialiseConnection();
+                    return m_instance;
+            }
+            
+    }
+    
+    void Awake(){
+            m_instance = this;
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public IEnumerator materials(){
+		Debug.Log("Materials");
+		HTTP.Request materialsRequest = new HTTP.Request("GET", "http://i.materialise.com/web-api/materials?user=camilo@thinkerthing.com");
+		materialsRequest.SetHeader("Accept", "application/json");
+        materialsRequest.SetHeader("Content-type", "application/x-www-form-urlencoded");
+		materialsRequest.Send();
+		
+		while(!materialsRequest.isDone) yield return new WaitForEndOfFrame();
+                
+        if (materialsRequest.exception != null){
+			Debug.LogError(materialsRequest.exception.ToString());
+		}
+                 
+        else{
+			Debug.Log(materialsRequest.response.Text);
+		}
 	}
 }
