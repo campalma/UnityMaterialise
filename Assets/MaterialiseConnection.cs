8896d6e1034feaa69c667f2fc0b78c857ef2b5fa
Camilo Palma <cpalmapradena@gmail.com>
2013-10-17 14:44:46 -0300

    Keys and Urls refactor

--- Assets/MaterialiseConnection.cs
+++ Assets/MaterialiseConnection.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MaterialiseConnection : MonoBehaviour {
 
  private static MaterialiseConnection m_instance;
         
     public static MaterialiseConnection Instance{
-            get{
-                    if(m_instance == null)
-                            m_instance = new MaterialiseConnection();
-                    return m_instance;
-            }
-            
+        get{
+            if(m_instance == null)
+                    m_instance = new MaterialiseConnection();
+            return m_instance;
+        }
     }
     
     void Awake(){
@@ -20,7 +20,9 @@ public class MaterialiseConnection : MonoBehaviour {
 	
 	public IEnumerator materials(){
 		Debug.Log("Materials");
-		HTTP.Request materialsRequest = new HTTP.Request("GET", "http://i.materialise.com/web-api/materials?user=camilo@thinkerthing.com");
+		Dictionary<string,string> parameters = new Dictionary<string, string>();
+		parameters.Add("user", MaterialiseKeys.EMAIL);
+		HTTP.Request materialsRequest = new HTTP.Request("GET", MaterialiseUrls.MATERIALS_URL+toQueryString(parameters));
 		materialsRequest.SetHeader("Accept", "application/json");
         materialsRequest.SetHeader("Content-type", "application/x-www-form-urlencoded");
 		materialsRequest.Send();
@@ -34,5 +36,13 @@ public class MaterialiseConnection : MonoBehaviour {
         else{
 			Debug.Log(materialsRequest.response.Text);
 		}
+	}
+	
+	public static string toQueryString(Dictionary<string, string> parameters){
+    	List<string> a = new List<string>();
+        foreach(KeyValuePair<string, string> pair in parameters){                        
+        	a.Add(pair.Key+"="+WWW.EscapeURL(pair.Value));
+		}
+        return "?" + string.Join("&", a.ToArray());
 	}
 }
---
+++ Assets/MaterialiseUrls.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialiseUrls{
+	public const string MATERIALS_URL = "http://i.materialise.com/web-api/materials";
+}
